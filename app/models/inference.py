0b636224367f0fb81cae51742959102a7a76adbb
Martin Oywa <martinoywa2@gmail.com>
2021-08-02 07:11:38 +0300

    Update inference file Remove preprocessing code

--- app/models/inference.py
+++ app/models/inference.py
@@ -1,50 +1,32 @@
 from .loaders import *
+from .preprocessor import *
 import torch
-from torchvision import transforms
-from transformers import BertTokenizer
-from PIL import Image
+from pathlib import Path
 
 
-audio_model = load_audio_model()
-lyrics_model = load_lyrics_model()
+audio_weights = Path("app/models/weights/audio/finetuned_Full-RESNET_model.pt")
+lyrics_weights = Path("app/models/weights/lyrics/finetuned_BERT_model-epoch-v.3.pt")
 
-audio_transforms = transforms.Compose([
-    transforms.Resize(256),
-    transforms.ToTensor()
-])
-
-lyrics_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased",
-                                                 do_lower_case=True)
+audio_model = load_audio_model(audio_weights)
+lyrics_model = load_lyrics_model(lyrics_weights)
 
 
 def audio_inference(spectrogram):
-    image = Image.open(spectrogram).convert("RGB")
-    input = audio_transforms(image).unsqueeze(0)
+    input = audio_preprocessor(spectrogram)
     output = audio_model(input)
-
     return output
 
 
 def lyrics_inference(lyrics):
-    encoded_input = lyrics_tokenizer.encode_plus(
-        lyrics,
-        add_special_tokens=True,
-        return_attention_mask=True,
-        padding=True,
-        truncation=True,
-        max_length=64,
-        return_tensors="pt"
-    )
-    output = lyrics_model(**encoded_input)
-
+    input = lyrics_preprocessor(lyrics)
+    output = lyrics_model(**input)
     return output
 
 
 def aggregate_inference(spectrogram, lyrics):
     audio_output = audio_inference(spectrogram)
     lyrics_output = lyrics_inference(lyrics)
-    aggregate = audio_output + lyrics_output
-    _, pred = torch.max(aggregate[0], dim=1)
+    aggregate = audio_output + lyrics_output[0]
+    _, pred = torch.max(aggregate, dim=1)
     pred = pred.item()+1
-
     return pred
