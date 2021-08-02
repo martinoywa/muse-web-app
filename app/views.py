e171e2a7f69674d18aee060210cd4c9c73f40220
Martin Oywa <martinoywa2@gmail.com>
2021-08-02 04:47:40 +0300

    Update base application

--- app/views.py
+++ app/views.py
@@ -1,37 +1,14 @@
-from flask import Blueprint
+from flask import Flask
 
 
-home = Blueprint("home", __name__)
-@home.route("/")
+app = Flask(__name__)
+
+
+@app.route("/")
 def index():
     return "Home Page"
 
 
-login = Blueprint("login", __name__)
-@login.route("/login")
-def user_login():
-    return "Log In Page"
-
-
-signup = Blueprint("signup", __name__)
-@signup.route("/signup")
-def user_signup():
-    return "Sign Up Page"
-
-
-details = Blueprint("details", __name__)  # song details
-@details.route("/details/id")
-def song_details(id):
-    return "Details Page"
-
-
-list = Blueprint("list", __name__)  # list per quadrant
-@list.route("/list/id")
-def quadrant_list(id):
-    return "List Page"
-
-
-upload = Blueprint("upload", __name__)
-@upload.route("/upload")
-def song_upload():
+@app.route("/upload")
+def upload():
     return "Upload Page"
