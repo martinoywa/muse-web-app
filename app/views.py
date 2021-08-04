13e6e7c4baee0960e4995a9d8a319eb11297a0fa
Martin Oywa <martinoywa2@gmail.com>
2021-08-04 20:48:19 +0300

    Update playlist html file. Update playlist route to include cluster details.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
-from .models import Songs
+from .models import Track
 from app import db
+
+from random import randint
+
+random_quadrant = randint(1, 4)
 
 
 main = Blueprint("main", __name__)
@@ -15,8 +19,13 @@ def index():
 @main.route("/playlist")
 @login_required
 def playlist():
-    list = Songs.query.all()
-    return render_template("playlist.html", list=list)
+    list_q1 = Track.query.filter_by(quadrant=1)
+    list_q2 = Track.query.filter_by(quadrant=2)
+    list_q3 = Track.query.filter_by(quadrant=3)
+    list_q4 = Track.query.filter_by(quadrant=4)
+
+    return render_template("playlist.html", list_q1=list_q1, list_q2=list_q2,
+                           list_q3=list_q3, list_q4=list_q4)
 
 @main.route("/upload", methods=["GET", "POST"])
 @login_required
@@ -27,14 +36,16 @@ def upload():
         album = request.form.get("album")
         lyrics = request.form.get("lyrics")
         song_file = request.form.get("song")
+        quadrant = random_quadrant
 
-        song = Songs.query.filter_by(title=title).first()
+        song = Track.query.filter_by(title=title).first()
 
         if song:
             flash("Song by already exists")
             return redirect(url_for("main.upload"))
 
-        new_song = Songs(title=title, name=name, album=album, lyrics=lyrics)
+        new_song = Track(quadrant=quadrant, title=title, name=name, album=album,
+                         lyrics=lyrics)
 
         db.session.add(new_song)
         db.session.commit()
